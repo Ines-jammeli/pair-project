806c70576bfd1edef592414176729a13d0909443
Ines-jammeli <jammeli.ines@gmail.com>
2021-07-10 16:46:53 +0200

    updated

---
+++ client/initialization.js
@@ -0,0 +1,38 @@
+
+var arbiList=[
+{id: '3arbi01', src: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Brebis_Barbarine.JPG", breed: "3arbi", weight: 43, height: 90, price: 750, age: 7 },
+{id: '3arbi02', src: "https://www.researchgate.net/profile/Khaldi-Zahrane/publication/259417114/figure/fig2/AS:297250275250181@1447881396277/Belier-de-Race-Queue-Fine-de-l-Ouest_Q320.jpg", breed: "3arbi", weight: 47, height: 86, price: 710 , age: 7},
+{id: '3arbi03', src: "http://www.algerlablanche.com/public/algerie/races_ovines_algerie/barbarine.jpg", breed: "3arbi", weight: 46, height: 45, price: 690, age: 9 },
+{id: '3arbi04', src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Mouton-rouge-du-roussillon_SDA2013.JPG/1200px-Mouton-rouge-du-roussillon_SDA2013.JPG", breed: "3arbi", weight: 41, height: 98, price: 630, age: 8 }
+]
+
+var gharbiList=[
+{id: 'gharbi01', src: "http://www.algerlablanche.com/public/algerie/races_ovines_algerie/chellalia.jpg", breed: "Gharbi", weight: 49, height: 70, price: 700, age: 6 },
+{id: 'gharbi02', src: "https://www.mdpi.com/animals/animals-11-00839/article_deploy/html/images/animals-11-00839-g003.png", breed: "Gharbi", weight: 46, height: 77, price: 640 , age: 12},
+{id: 'gharbi03', src: "https://www.echoroukonline.com/wp-content/uploads/2021/03/kebch_342632160.jpg", breed: "Gharbi", weight: 43, height: 94, price: 600, age: 9 },
+{id: 'gharbi04', src: "https://tunisie.co/uploads/images/content/aid-280817-1.jpg", breed: "Gharbi", weight: 45, height: 106, price: 670, age: 8 }
+]
+
+
+
+var ThibarList=[
+{id: 'Thibar01', src: "https://docplayer.fr/docs-images/91/105651171/images/34-0.jpg", breed: "Thibari", weight: 41, height: 70, price: 450, age: 9 },
+{id: 'Thibar02', src: "http://tunisie.co/uploads/images/content/mouton-090916-6.jpg", breed: "Thibari", weight: 43, height: 77, price: 420 , age: 7},
+{id: 'Thibar03', src: "https://www.races-ovines-des-massifs.com/fr/description-races/noire-velay/images/noire-velay.jpg", breed: "Thibari", weight: 42, height: 94, price: 550, age: 8 },
+{id: 'Thibar04', src: "http://www.location-mouton.ovh/images/mouton_page1_1.jpg", breed: "Thibari", weight: 46, height: 100, price: 630, age: 12 }
+]
+
+var dhabe7List=[
+{id: 'Dhabe701', src: "https://c8.alamy.com/compfr/k22b17/paris-france-26-juin-2017-coupe-boucher-musulman-un-mouton-pour-l-aid-al-adha-l-eid-al-adha-fete-du-sacrifice-est-le-deuxieme-de-deux-holid-musulmane-k22b17.jpg", name: "3am Salah", price: 40, location: 'Ariana' },
+{id: 'Dhabe702', src: "https://www.france-hajj.fr/wp-content/uploads/2016/09/hajj-aid-al-adha-1.jpg", name: "3am Me7rez", price: 30, location: 'Beja' },
+{id: 'Dhabe703', src: "https://aujourdhui.ma/wp-content/uploads/2016/02/cest-pour-son-professionnalisme-que-lon-choisit-le-boucher-pour-decouper-la-carcasse-du-mouton-de-laid-300x224.jpg", name: "3am Menji", price: 30, location: 'Sfax' },
+{id: 'Dhabe704', src: "https://www.al-kanz.org/wp-content/uploads/2020/06/boucherie-halal.jpg", name: "3am Mounir", price: 35, location: 'Sousse' },
+{id: 'Dhabe705', src: "3andi_dhabe7.png", name: '', price: 0, location: '' },
+]
+
+
+localStorage.setItem("arbiList", JSON.stringify(arbiList));
+localStorage.setItem("gharbiList", JSON.stringify(gharbiList));
+localStorage.setItem("ThibarList", JSON.stringify(ThibarList));
+localStorage.setItem("dhabe7List", JSON.stringify(dhabe7List));
+
